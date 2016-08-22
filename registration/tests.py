380f29362d4b5b441b207be3eecb742d6911a9e5
DummyTrip <mladiot@gmail.com>
2016-08-22 16:23:34 +0200

    User registration done

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class NewUserTest(TestCase):
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/accounts/register/',
+            data={'username': 'test',
+                  'email': 'test@test.test',
+                  'password': 'testtest'}
+        )
+
+        self.assertEqual(User.objects.count(), 1)
+        user = User.objects.first()
+        self.assertEqual(user.username, 'test')
--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        User.objects.create_user(username, email, password)
+
+        return redirect('/')
     return render(request, 'registration/register.html')
 
 
